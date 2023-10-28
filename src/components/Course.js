107a9df8d951b3564aaf1d1cef32a435de0c41c4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-28 09:15:15 +0100

    edited files

--- src/components/Course.js
+++ src/components/Course.js
@@ -3,23 +3,32 @@ import styled from 'styled-components';
 import Hole from './Hole';
 
 const StyledCourse = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: green;
+  width: 100%;
+  height: 100%;
+  background-color: green;
 `;
 
 const Course = ({ physics }) => {
   const [currentHole, setCurrentHole] = useState(1);
+  const [courseCompleted, setCourseCompleted] = useState(false);
 
   const handleHoleCompleted = () => {
-    // Check if the player has completed the course
     if (currentHole === 18) {
-      // The player has completed the course!
-      // TODO: Show a victory screen or something
+      setCourseCompleted(true);
       return;
     }
 
     setCurrentHole(currentHole + 1);
+  };
+
+  const VictoryScreen = () => {
+    return (
+      <div>
+        <h1>You win!</h1>
+        <p>You completed the course in {currentHole} strokes.</p>
+        <button onClick={() => setCurrentHole(1)}>Play again</button>
+      </div>
+    );
   };
 
   return (
@@ -29,8 +38,11 @@ const Course = ({ physics }) => {
         number={currentHole}
         onHoleCompleted={handleHoleCompleted}
       />
+      {courseCompleted && <VictoryScreen />}
     </StyledCourse>
   );
 };
+
+
 
 export default Course;
